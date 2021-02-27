6e5641e15f7f7ee0de97f80fbb1cb41b846dd616
Rutger Nieuwenhuis <rn222np@student.lnu.se>
2021-02-27 17:49:34 +0100

    removed unnecessary columns from artworks csv file

---
+++ csv_column_deletion.py
@@ -0,0 +1,15 @@
+import csv
+
+# https://stackoverflow.com/questions/7588934/how-to-delete-columns-in-a-csv-file
+# Keep these columns from the detailed description of artworks (artworks_detailed.csv), and write the result to artworks.csv
+columns_to_keep = ["Title", "Artist", "Date", "Medium", "Dimensions", "URL", "Circumference (cm)", "Depth (cm)", "Diameter (cm)", "Height (cm)", "Length (cm)", "Weight (kg)", "Width (cm)", "Seat Height (cm)", "Duration (sec.)"]
+
+with open("artworks_original.csv", encoding="utf-8-sig") as f:
+    original = list(csv.reader(f, delimiter=','))
+    indexes_to_keep = [original[0].index(column) for column in columns_to_keep] 
+
+    with open("artworks.csv", "w") as f2:
+        write_file = csv.writer(f2)
+        for row in original:
+            row_length = len(row)
+            write_file.writerow( (row[i] for i in indexes_to_keep if i < row_length) )
