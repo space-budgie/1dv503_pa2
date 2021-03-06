5655223dbfba2f9fb9b85370e8195d80475b7445
Rutger Nieuwenhuis <rn222np@student.lnu.se>
2021-03-06 15:00:58 +0100

    created user client interface

--- src/database_creation.py
+++ src/database_creation.py
@@ -1,9 +1,12 @@
-import mysql.connector
 from mysql.connector import errorcode
 
 cnx = mysql.connector.connect(user='root', password='root',
                               host='127.0.0.1')
+                              auth_plugin='mysql_native_password')
 
 DB_NAME = "AuctionHouse"
-
+cursor = cnx.cursor()
+cursor.execute("USE AuctionHouseDB")
+cursor.execute("SELECT * FROM Buyer")
+print(cursor.column_names)
 
