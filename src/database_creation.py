35da7752bb5ac54aea9666bce1daf066588c8e12
Paolo Molinaro <pm222py@student.lnu.se>
2021-03-06 16:39:49 +0100

    crate db from dump, fixed incompatibility dumpsql

--- src/database_creation.py
+++ src/database_creation.py
@@ -1,12 +1,22 @@
 from mysql.connector import errorcode
+import mysql.connector
+
+#Source: https://stackoverflow.com/questions/34275794/how-do-i-import-a-mysql-database-in-a-python-script
+
+
+# cnx = mysql.connector.connect(user='root', password='root',
+#                               host='127.0.0.1',
+#                               auth_plugin='mysql_native_password')
 
 cnx = mysql.connector.connect(user='root', password='root',
-                              host='127.0.0.1')
-                              auth_plugin='mysql_native_password')
+                              unix_socket= '/Applications/MAMP/tmp/mysql/mysql.sock')
 
-DB_NAME = "AuctionHouse"
 cursor = cnx.cursor()
-cursor.execute("USE AuctionHouseDB")
-cursor.execute("SELECT * FROM Buyer")
-print(cursor.column_names)
+
+file = open('data/SQLs/DatabaseDump.sql')
+sql = file.read()
+
+for result in cursor.execute(sql, multi=True):
+  if result.with_rows:
+    result.fetchall()
 
